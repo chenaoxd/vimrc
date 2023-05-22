0a4e955107306cb9432dbce354446033449905ab
Ao Chen <chenao3220@gmail.com>
2023-05-23 01:12:47 +0800

    Use relative path in inactive pane

--- init.lua
+++ init.lua
@@ -78,7 +78,12 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      }
+    },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
