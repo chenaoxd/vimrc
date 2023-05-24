a5b712b250b1294369607ac94b0a730be3ee6fa4
Ao Chen <chenao3220@gmail.com>
2023-05-24 15:50:01 +0800

    Improve nvim-tree width

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ require("nvim-tree").setup()
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
-    width = 30,
+    width = 60,
   },
   renderer = {
     group_empty = true,
